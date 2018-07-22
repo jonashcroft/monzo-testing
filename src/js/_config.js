14205e7eb190da1eaad7eb530777557bd99bb024
Jon Ashcroft <hello@jonashcroft.co.uk>
2018-07-22 20:51:31 +0100

    Add: new config.js

---
+++ src/js/_config.js
@@ -0,0 +1,11 @@
+const config = {
+    'clientId'     : '** CLIENT ID **',
+    'clientSecret' : '** CLIENT SECRET **',
+
+    'redirectUrl'  : 'http://localhost:3000',
+
+    'connectedDiv' :  document.getElementById('connected'),
+    'authDiv'      :  document.getElementById('authDiv')
+}
+
+export default config;
