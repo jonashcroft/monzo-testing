0d866327e02f38db01b289b2ce527df947c1b458
Jon Ashcroft <hello@jonashcroft.co.uk>
2018-08-11 13:53:56 +0100

    Add monzo endpoint as config var

--- src/js/_config.js
+++ src/js/_config.js
@@ -3,6 +3,7 @@ const config = {
     'clientSecret' : '** CLIENT SECRET **',
 
     'redirectUrl'  : 'http://localhost:3000',
+    'monzoUrl'     : 'https://api.monzo.com',
 
     'connectedDiv' :  document.getElementById('connected'),
     'authDiv'      :  document.getElementById('authDiv')
